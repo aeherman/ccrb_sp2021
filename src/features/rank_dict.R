8a1119f5c4331f80def0a6b569c35144da843d52
aeherman <aeh2196@columbia.edu>
2021-04-06 19:37:11 -0400

    organize src

---
+++ src/features/rank_dict.R
@@ -0,0 +1,27 @@
+# Calculate Numeric Values of Officer Ranks
+
+## read in relevant data
+
+
+## confirm that ranks and abbreviations make sense at time of incident...
+a %>% left_join(rank, by = c("rank_abbrev_incident" = "Abbreviation")) %>% group_by(rank_incident) %>%
+  summarize(ranks = paste(unique(Rank), collapse = ", "))
+## and now:
+a %>% left_join(rank, by = c("rank_abbrev_now" = "Abbreviation")) %>% group_by(rank_now) %>%
+  summarize(ranks = paste(unique(Rank), collapse = ", "))
+## code numbers for ranks
+rank$Rank <- str_to_lower(rank$Rank)
+rank_dict <- rank %>%
+  # record the order of the categories
+  mutate(row = row_number()) %>%
+  # filter for relevant categories
+  filter(Abbreviation %in% c(unique(a$rank_abbrev_incident, a$rank_abbrev_now))) %>%
+  # regroup through string matching
+  group_by(rank_cat = case_when(
+    str_detect(Rank, "chief") ~ "chiefs and other ranks",
+    str_detect(Rank, "deputy|police") ~ Rank,
+    TRUE ~ word(Rank, 1))) %>%
+  # select only first item in each new category
+  transmute(rank = row_number()) %>% filter(rank == 1) %>%
+  # record rank
+  ungroup() %>% mutate(rank = row_number())
